44d96bf02d8e4b64b320a79e1731caa7b65f131c
BOUSSAID Rabah <boussaidrabah30@gmail.com>
2025-01-12 11:09:24 +0100

    mon projet jusqu'au panier

--- tests/RegisterUserTest.php
+++ tests/RegisterUserTest.php
@@ -8,10 +8,25 @@ class RegisterUserTest extends WebTestCase
 {
     public function testSomething(): void
     {
+        //Créer un client (navigateur web) de pointer vers un URL 
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $client->request('GET', '/inscription');
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+        //remplir un formulaire d'inscription
+        $client->submitForm('S\'inscrire', [
+            'register_user[email]' => 'test@example.com',
+            'register_user[plainPassword][first]' => '12345678',
+            'register_user[plainPassword][second]' => '12345678',
+            'register_user[firstname]' => 'Rabah',
+            'register_user[lastname]' => 'rabah'
+
+        ]);
+
+        //Follower(suivre les redirections) le lien de confirmation
+        $this->assertResponseRedirects('/connexion'); //test redirect(tester la redirection du confirmation) 
+        $client->followRedirect();
+
+        //vérifier que le formulaire est bien rempli et valide (message d'alerte de champs requis)
+        $this->assertSelectorExists('div:contains("Votre compte a bien été créée. Vous pouvez vous connecter maintenant.")');   
     }
 }
